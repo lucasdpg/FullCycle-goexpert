9d0e8add69337db4e0946b889c92671e9db1c560
Lucas Gomes <lucasgomes@duck.local>
2024-06-27 23:13:05 -0300

    Multithreading Challenge - chanels

--- Multithreading/main.go
+++ Multithreading/main.go
@@ -3,60 +3,55 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 )
 
 func main() {
-	cep := "01153000"
-	//url1 := "https://viacep.com.br/ws/"
-	//compl1 := "/json/"
-	url2 := "https://brasilapi.com.br/api/cep/v1/"
-	compl2 := ""
 
-	body, err := reqCep(cep, url2, compl2)
+	cep := "01153000"
+
+	url1 := "https://viacep.com.br/ws/"
+	compl1 := "/json/"
+	resultChan1 := make(chan string)
+
+	//url2 := "https://brasilapi.com.br/api/cep/v1/"
+	//compl2 := ""
+
+	go func() {
+		body, err := reqCep(cep, url1, compl1)
+		if err != nil {
+			panic(err)
+		}
+		resultChan1 <- body
+	}()
+
+	res := <-resultChan1
+
+	fmt.Println(string(res))
+}
+
+func reqCep(cep, url, compl string) (string, error) {
+
+	req, err := http.Get(url + cep + compl)
 	if err != nil {
-		fmt.Println("Erro:", err)
-		return
+		return "", err
+	}
+	defer req.Body.Close()
+
+	if req.StatusCode != http.StatusOK {
+		return "", err
 	}
 
-	jsonData, err := readBody(body)
-	if err != nil {
-		fmt.Println("Error to read response:", err)
-		return
+	var jsonData map[string]interface{}
+	if err := json.NewDecoder(req.Body).Decode(&jsonData); err != nil {
+		return "", err
 	}
 
 	response, err := json.MarshalIndent(jsonData, "", "  ")
 	if err != nil {
-		fmt.Println("Error to marshal JSON:", err)
-		return
+		return "", err
 	}
 
-	fmt.Println(string(response))
-}
-
-func reqCep(cep, url, compl string) (io.ReadCloser, error) {
-	req, err := http.Get(url + cep + compl)
-	if err != nil {
-		return nil, fmt.Errorf("error to get CEP %v: %v", cep, err)
-	}
-
-	if req.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("error to get CEP %v: status %v", cep, req.Status)
-	}
-
-	return req.Body, nil
-}
-
-func readBody(body io.ReadCloser) (map[string]interface{}, error) {
-
-	defer body.Close()
-
-	var result map[string]interface{}
-	if err := json.NewDecoder(body).Decode(&result); err != nil {
-		return nil, fmt.Errorf("error to decode body: %v", err)
-	}
-
-	return result, nil
+	return string(response), nil
 
 }
