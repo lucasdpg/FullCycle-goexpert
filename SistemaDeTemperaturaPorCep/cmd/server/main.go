0be5f2659040997b43572b4dfd54be940c9258e8
Lucas Gomes <lucasdavip@hotmail.com>
2024-09-17 21:29:20 -0300

    Parte 2

--- SistemaDeTemperaturaPorCep/cmd/server/main.go
+++ SistemaDeTemperaturaPorCep/cmd/server/main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/lucasdpg/FullCycle-goexpert/SistemaDeTemperaturaPorCep/configs"
+	"github.com/lucasdpg/FullCycle-goexpert/SistemaDeTemperaturaPorCep/pkg/services"
 )
-
-type Temperature struct {
-	Temp_c float64 `json:"temp_c"`
-	Temp_f float64 `json:"temp_f"`
-	Temp_k float64
-}
 
 func main() {
 
@@ -31,17 +25,21 @@ func main() {
 		cep := chi.URLParam(r, "cep")
 
 		if cep == "" {
-			http.Error(w, "CEP não fornecido", http.StatusBadRequest)
+			http.Error(w, "invalid zipcode", http.StatusUnprocessableEntity)
+			return
+		}
+		if !services.ValidateZipcode(cep) {
+			http.Error(w, "invalid zipcode", http.StatusUnprocessableEntity)
 			return
 		}
 
-		lat, lon, err := getLatLonByCep(cep)
+		lat, lon, err := services.GetLatLonByZipcode(cep)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
+			http.Error(w, err.Error(), http.StatusNotFound)
 			return
 		}
 
-		currentWeather, err := getCurrentWeather(lat, lon, configs.WeatherApiToken)
+		currentWeather, err := services.GetCurrentWeather(lat, lon, configs.WeatherApiToken)
 		if err != nil {
 			http.Error(w, "Error Weather: "+err.Error(), http.StatusBadRequest)
 			return
@@ -55,68 +53,4 @@ func main() {
 	})
 
 	http.ListenAndServe(":3000", r)
-}
-
-func getLatLonByCep(cep string) (string, string, error) {
-	url := fmt.Sprintf("https://nominatim.openstreetmap.org/search?postalcode=%s&country=Brazil&format=json", cep)
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", "", err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return "", "", fmt.Errorf("falha na solicitação: status %d", resp.StatusCode)
-	}
-
-	var jsonData []struct {
-		Lat string `json:"lat"`
-		Lon string `json:"lon"`
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&jsonData); err != nil {
-		return "", "", err
-	}
-
-	if len(jsonData) == 0 {
-		return "", "", fmt.Errorf("nenhum dado encontrado")
-	}
-
-	return jsonData[0].Lat, jsonData[0].Lon, nil
-}
-
-func getCurrentWeather(lat, lon, apikey string) (*Temperature, error) {
-
-	url := fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s&q=%s,%s&aqi=no", apikey, lat, lon)
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("falha na solicitação: status %d", resp.StatusCode)
-	}
-
-	var jsonData struct {
-		Current struct {
-			Temp_c float64 `json:"temp_c"`
-			Temp_f float64 `json:"temp_f"`
-		} `json:"current"`
-	}
-
-	if err := json.NewDecoder(resp.Body).Decode(&jsonData); err != nil {
-		return nil, err
-	}
-
-	temp_K := (jsonData.Current.Temp_c + 273.15)
-
-	temp := &Temperature{
-		Temp_c: jsonData.Current.Temp_c,
-		Temp_f: jsonData.Current.Temp_f,
-		Temp_k: temp_K,
-	}
-
-	return temp, nil
 }
