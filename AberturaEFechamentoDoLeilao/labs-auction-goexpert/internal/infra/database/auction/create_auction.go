2380a28993b295caaf226cc0348d9798c3f1e7e6
Lucas Gomes <lucasdavip@hotmail.com>
2024-10-29 20:41:31 -0300

    Init AberturaEFechamentoDoLeilao

---
+++ AberturaEFechamentoDoLeilao/labs-auction-goexpert/internal/infra/database/auction/create_auction.go
@@ -0,0 +1,52 @@
+package auction
+
+// Concentre-se na no arquivo internal/infra/database/auction/create_auction.go, você deverá implementar a solução nesse arquivo
+
+import (
+	"context"
+	"fullcycle-auction_go/configuration/logger"
+	"fullcycle-auction_go/internal/entity/auction_entity"
+	"fullcycle-auction_go/internal/internal_error"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type AuctionEntityMongo struct {
+	Id          string                          `bson:"_id"`
+	ProductName string                          `bson:"product_name"`
+	Category    string                          `bson:"category"`
+	Description string                          `bson:"description"`
+	Condition   auction_entity.ProductCondition `bson:"condition"`
+	Status      auction_entity.AuctionStatus    `bson:"status"`
+	Timestamp   int64                           `bson:"timestamp"`
+}
+type AuctionRepository struct {
+	Collection *mongo.Collection
+}
+
+func NewAuctionRepository(database *mongo.Database) *AuctionRepository {
+	return &AuctionRepository{
+		Collection: database.Collection("auctions"),
+	}
+}
+
+func (ar *AuctionRepository) CreateAuction(
+	ctx context.Context,
+	auctionEntity *auction_entity.Auction) *internal_error.InternalError {
+	auctionEntityMongo := &AuctionEntityMongo{
+		Id:          auctionEntity.Id,
+		ProductName: auctionEntity.ProductName,
+		Category:    auctionEntity.Category,
+		Description: auctionEntity.Description,
+		Condition:   auctionEntity.Condition,
+		Status:      auctionEntity.Status,
+		Timestamp:   auctionEntity.Timestamp.Unix(),
+	}
+	_, err := ar.Collection.InsertOne(ctx, auctionEntityMongo)
+	if err != nil {
+		logger.Error("Error trying to insert auction", err)
+		return internal_error.NewInternalServerError("Error trying to insert auction")
+	}
+
+	return nil
+}
